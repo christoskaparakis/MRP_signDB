515a1904b61cc812b0e2a1bc82de64ddca2b6a26
diegodibe <40866333+diegodibe@users.noreply.github.com>
2021-01-14 15:13:52 +0100

    Add files via upload

---
+++ alignment/alignment_module.py
@@ -0,0 +1,6 @@
+from alignment.video_audio_segmentation import video_audio_segmentation
+
+
+def alignment_module():
+    path = r'alignment'
+    video_audio_segmentation(path)
---
+++ alignment/graph_processing.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import json
+
+# segmentation values
+units = 10
+graphs_per_unit = 3
+
+# load data
+
